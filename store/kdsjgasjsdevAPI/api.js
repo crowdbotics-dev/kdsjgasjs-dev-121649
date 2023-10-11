b8083f7952540da9cbb4f59ff28713830617f9c1
Crowdbotics <engineering@crowdbotics.com>
2023-10-11 11:46:13 +0000

    Update store.

--- store/kdsjgasjsdevAPI/api.js
+++ store/kdsjgasjsdevAPI/api.js
@@ -3,34 +3,16 @@ const kdsjgasjsdevAPI = axios.create({
   baseURL: "https://kdsjgasjs-dev-121649.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-function api_v1_fghj_list(payload) {
-  return kdsjgasjsdevAPI.get(`/api/v1/fghj/`)
-}
-function api_v1_fghj_create(payload) {
-  return kdsjgasjsdevAPI.post(`/api/v1/fghj/`, payload)
-}
-function api_v1_login_create(payload) {
-  return kdsjgasjsdevAPI.post(`/api/v1/login/`, payload)
-}
-function api_v1_signup_create(payload) {
-  return kdsjgasjsdevAPI.post(`/api/v1/signup/`, payload)
-}
-function rest_auth_user_retrieve(payload) {
-  return kdsjgasjsdevAPI.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(payload) {
-  return kdsjgasjsdevAPI.put(`/rest-auth/user/`, payload)
-}
-function rest_auth_user_partial_update(payload) {
-  return kdsjgasjsdevAPI.patch(`/rest-auth/user/`, payload)
-}
 function api_docs_schema_retrieve(payload) {
   return kdsjgasjsdevAPI.get(`/api-docs/schema/`, {
     params: { lang: payload.lang }
   })
 }
-function rest_auth_login_create(payload) {
-  return kdsjgasjsdevAPI.post(`/rest-auth/login/`, payload)
+function api_v1_fghj_list(payload) {
+  return kdsjgasjsdevAPI.get(`/api/v1/fghj/`)
+}
+function api_v1_fghj_create(payload) {
+  return kdsjgasjsdevAPI.post(`/api/v1/fghj/`, payload)
 }
 function api_v1_fghj_retrieve(payload) {
   return kdsjgasjsdevAPI.get(`/api/v1/fghj/${payload.id}/`)
@@ -44,14 +26,14 @@ function api_v1_fghj_partial_update(payload) {
 function api_v1_fghj_destroy(payload) {
   return kdsjgasjsdevAPI.delete(`/api/v1/fghj/${payload.id}/`)
 }
+function api_v1_login_create(payload) {
+  return kdsjgasjsdevAPI.post(`/api/v1/login/`, payload)
+}
 function api_v1_sdfddfbfd_list(payload) {
   return kdsjgasjsdevAPI.get(`/api/v1/sdfddfbfd/`)
 }
 function api_v1_sdfddfbfd_create(payload) {
   return kdsjgasjsdevAPI.post(`/api/v1/sdfddfbfd/`, payload)
-}
-function rest_auth_logout_create(payload) {
-  return kdsjgasjsdevAPI.post(`/rest-auth/logout/`)
 }
 function api_v1_sdfddfbfd_retrieve(payload) {
   return kdsjgasjsdevAPI.get(`/api/v1/sdfddfbfd/${payload.id}/`)
@@ -65,17 +47,26 @@ function api_v1_sdfddfbfd_partial_update(payload) {
 function api_v1_sdfddfbfd_destroy(payload) {
   return kdsjgasjsdevAPI.delete(`/api/v1/sdfddfbfd/${payload.id}/`)
 }
-function rest_auth_registration_create(payload) {
-  return kdsjgasjsdevAPI.post(`/rest-auth/registration/`, payload)
+function api_v1_signup_create(payload) {
+  return kdsjgasjsdevAPI.post(`/api/v1/signup/`, payload)
 }
-function rest_auth_password_reset_create(payload) {
-  return kdsjgasjsdevAPI.post(`/rest-auth/password/reset/`, payload)
+function rest_auth_login_create(payload) {
+  return kdsjgasjsdevAPI.post(`/rest-auth/login/`, payload)
+}
+function rest_auth_logout_create(payload) {
+  return kdsjgasjsdevAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
   return kdsjgasjsdevAPI.post(`/rest-auth/password/change/`, payload)
 }
+function rest_auth_password_reset_create(payload) {
+  return kdsjgasjsdevAPI.post(`/rest-auth/password/reset/`, payload)
+}
 function rest_auth_password_reset_confirm_create(payload) {
   return kdsjgasjsdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
+function rest_auth_registration_create(payload) {
+  return kdsjgasjsdevAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_resend_email_create(payload) {
   return kdsjgasjsdevAPI.post(`/rest-auth/registration/resend-email/`, payload)
@@ -83,31 +74,40 @@ function rest_auth_registration_resend_email_create(payload) {
 function rest_auth_registration_verify_email_create(payload) {
   return kdsjgasjsdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
+function rest_auth_user_retrieve(payload) {
+  return kdsjgasjsdevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return kdsjgasjsdevAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return kdsjgasjsdevAPI.patch(`/rest-auth/user/`, payload)
+}
 export const apiService = {
+  api_docs_schema_retrieve,
   api_v1_fghj_list,
   api_v1_fghj_create,
-  api_v1_login_create,
-  api_v1_signup_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update,
-  api_docs_schema_retrieve,
-  rest_auth_login_create,
   api_v1_fghj_retrieve,
   api_v1_fghj_update,
   api_v1_fghj_partial_update,
   api_v1_fghj_destroy,
+  api_v1_login_create,
   api_v1_sdfddfbfd_list,
   api_v1_sdfddfbfd_create,
-  rest_auth_logout_create,
   api_v1_sdfddfbfd_retrieve,
   api_v1_sdfddfbfd_update,
   api_v1_sdfddfbfd_partial_update,
   api_v1_sdfddfbfd_destroy,
-  rest_auth_registration_create,
-  rest_auth_password_reset_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_create,
   rest_auth_password_change_create,
+  rest_auth_password_reset_create,
   rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
   rest_auth_registration_resend_email_create,
-  rest_auth_registration_verify_email_create
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
 }
