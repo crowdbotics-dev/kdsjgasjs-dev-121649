f659c3f04335b49a1184aa03bfdc63ec9ad0f1e7
crowdbotics-dev <engineering@crowdbotics.com>
2023-10-11 07:05:16 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import FGHj
+admin.site.register(FGHj)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+class FGHj(models.Model):
+    'Generated Model'
+    hvjh = models.CharField(max_length=255,)
+    dcsd = models.BigIntegerField(null=True,blank=True,)
+    dfsb = models.DateTimeField(null=True,blank=True,)
+    seffsv = models.TextField(null=True,blank=True,)
+    afewreg = models.BinaryField(null=True,blank=True,)
+    sfgebfg = models.SmallIntegerField(null=True,blank=True,)
+    rel_user_1_1 = models.OneToOneField("users.User",blank=True,null=True,on_delete=models.CASCADE,related_name="fghj_rel_user_1_1",)
+    rel_user_1_n = models.ForeignKey("users.User",blank=True,null=True,on_delete=models.CASCADE,related_name="fghj_rel_user_1_n",)
